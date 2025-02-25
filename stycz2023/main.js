b503f839d90546b1782817d02a4d697b0797351e
GrzegorzT <144318154+GrzTer@users.noreply.github.com>
2025-02-25 14:33:10 +0100

    .

--- stycz2023/main.js
+++ stycz2023/main.js
@@ -1,7 +1,6 @@
-
 function licz() {
-    let pow = parseFloat(document.getElementById('pow').value);
-    let wp = 3.5; 
+    const pow = parseFloat(document.getElementById('pow').value);
+    var wp = 4; 
     let lp = Math.ceil(pow / wp);
     document.getElementById('w').textContent = "Liczba puszek: " + lp;
 }
