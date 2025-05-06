802457ba7ae67f08557ba189489ac89ef40af8f2
GrzTer <grzegorz.tereszkiewicz@infotech.edu.pl>
2025-05-06 12:39:51 +0200

    Dom3

---
+++ Dom/ryby/wedkuj.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wędkowanie</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+<body>
+    <header>
+        <h1>Portal dla wędkarzy</h1>
+    </header>
+    <main>
+        <section class="lewy" id="lewy1">
+            <h3>Ryby zamieszkujące rzeki</h3>
+            <ol>
+                <?php
+                ?>
+            </ol>
+        </section>
+        <section class="lewy" id="lewy2">
+            <h3>Ryby drapieżne naszych wód</h3>
+            <table>
+                <tr>
+                    <th>L.p.</th>
+                    <th>Gatunek</th>
+                    <th>Występowanie</th>
+                </tr>
+                <?php
+                ?>
+            </table>
+        </section>
+        <section id="prawy">
+            <img src="ryba1.jpg" alt="Sum">
+            <a href="kwerendy.txt">Pobierz kwerendy</a>
+        </section>
+    </main>
+    <footer>
+        <p>Stronę wykonał: 000000000</p>
+    </footer>
+</body>
+</html>
